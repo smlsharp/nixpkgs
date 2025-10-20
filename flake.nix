5c80bbf1ccf34d170b1aed9949938b6fe1aceb4c
Katsuhiro Ueno <katsu@ie.niigata-u.ac.jp>
2025-10-20 13:37:33 +0900

    add devShells for developers' convenience

--- flake.nix
+++ flake.nix
@@ -16,6 +16,13 @@
     in
     {
       packages = nixpkgs.lib.mapAttrs (_: addDefault "smlsharp") packages;
+      devShells = nixpkgs.lib.mapAttrs (
+        system: packages:
+        packages // { default = nixpkgs.legacyPackages.${system}.mkShell {
+          inputsFrom = [ packages.smlsharp ];
+          packages = [ nixpkgs.legacyPackages.${system}.autoconf ];
+        }; }
+      ) packages;
       overlays = {
         packages = final: prev: pkgs final.system;
       };
